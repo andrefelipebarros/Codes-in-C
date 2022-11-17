77f1e33e5b3ad41fb13a8671a20b7ece912ac092
André Felipe de Barros A. N <100530782+andrefelipebarros@users.noreply.github.com>
2022-11-17 18:17:55 -0300

    Organizando a explicação do código
    comentando sobre meu código para a explicação alheia e para mim.

--- C++/Estudo-Switch,Do,While.cpp
+++ C++/Estudo-Switch,Do,While.cpp
@@ -7,7 +7,7 @@ int main(){
     int x;
     int y;
     char continua;
-    // início do código e declarando repetição "do"
+    // início do código e declarando comandos a serem repetidos pelo "do"
     do{
         // gerando lista de escolhas
         cout << "Bem vindo ao Mac Donalds!\n";
@@ -19,10 +19,11 @@ int main(){
         cout << "4 - Mc Fish\n";
         cout << "5 - Cheddar McMelt\n";
         cout << "Sua opção: ";
-        //linkando as escolhas a lista inteira de case denominada x
+        
+        //linkando as escolhas a lista do switch case denominada x
         cin >> x;
        
-       //declarando a lista por switch x
+       //declarando a lista do switch
         switch(x){
             case 1:
                 cout << "\nVocê escolheu Big Mac";
@@ -39,7 +40,7 @@ int main(){
             case 5:
                 cout << "\nVocê escolheu Cheddar McMelt";
                 break;
-        // para quem escolher uma case que n existe aparecerá normalmente essa mensagem:
+        // caso a escolha de uma das cases não exista aparecerá essa mensagem:
             default :
                 cout << "\nVocê escolheu uma opção inválida";
                 break;
@@ -56,32 +57,34 @@ int main(){
         cout << "4 - CocaColaZero\n";
         cout << "5 - Água\n";
         cout << "Sua opção: ";
-        cin >> y; //linkando a lista
+        
+        cin >> y;
         
         switch(y){
             case 1:
-            cout << "Você escolheu CocaCola\n";
+                cout << "Você escolheu CocaCola\n";
             break;
             case 2:
-            cout << "Você escolheu GuaranáAntartica\n";
+                cout << "Você escolheu GuaranáAntartica\n";
             break;
             case 3:
-            cout << "Você escolheu Pepsi\n";
+                cout << "Você escolheu Pepsi\n";
             break;
             case 4:
-            cout << "Você escolheu CocaColaZero\n";
+                cout << "Você escolheu CocaColaZero\n";
             break;
             case 5:
-            cout << "Você escolheu Água\n";
+                cout << "Você escolheu Água\n";
             break;
-            // para quem escolher uma case que n existe aparecerá normalmente essa mensagem:
+
             default :
                 cout << "\nVocê escolheu uma opção inválida";
                 break;
         }
-        //declarando a variável para a repetição
+        
         cout << "\nDeseja continuar escolhendo? (S/N) ";
         cin >> continua;
     }
+         //linkando while a variavel continua e que encerrará o código caso seja digitado "N" maiusculo ou minusculo
          while (continua != ('N','n'));
 }
